1ea33ca9db69d1d648b5adda0e5a55642a5cfffe
maverickwagley <guildedibis@gmail.com>
2025-02-20 22:28:18 -0500

    Start Module 7 Lab

--- Test/Test02.R
+++ Test/Test02.R
@@ -1,4 +1,5 @@
 #Libraries
+install.packages("bookdown")
 library(tidyverse)
 library(hrbrthemes)
 library(viridis)
@@ -8,7 +9,7 @@ setwd("C:/Users/maver/OneDrive/Documents/School/DS605/DSCI605_Labs/Datasets")
 data(mpg)
 
 
-#Scatter Plot
+#Basic Scatter Plot
 mpg %>%
   arrange(desc(class)) %>%
   ggplot(aes(x=cty,y=displ,fill=class,shape=as.factor(cyl))) + 
@@ -19,3 +20,16 @@ mpg %>%
   guides(fill=guide_legend(nrwo=2,byrow=TRUE,title="Class"))+
   ylab("Engine displacement, in litres (displ)") +
   xlab("City mile per gallon (cyt)")
+
+#Scatter PLot 2
+mpg %>%
+  arrange(desc(class)) %>%
+  ggplot(aes(x=cty,y=displ,size=year,fill=class)) + 
+  geom_point(alpha=0.5,shape=21,color="black") + 
+  scale_fill_viridis(discrete=TRUE,option="A") +
+  theme(legend.position ="bottom") +
+  guides(shape=guide_legend(nrow=2,byrow=TRUE,title="CYL"))+
+  guides(fill=guide_legend(nrwo=2,byrow=TRUE,title="Class"))+
+  ylab("Engine displacement, in litres (displ)") +
+  xlab("City mile per gallon (cyt)")
+  
