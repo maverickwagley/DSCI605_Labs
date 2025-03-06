73f75c5c94952724a1fffd3b8864bee091391c35
maverickwagley <guildedibis@gmail.com>
2025-03-05 22:37:51 -0500

    Module 9 Shiny App

--- Module9/MaverickWagley_Interactive plotting with Shiny app and plotly.R
+++ Module9/MaverickWagley_Interactive plotting with Shiny app and plotly.R
@@ -1,10 +1,19 @@
+#####
+# MaverickWagley_Interactive plotting with Shiny app and plotly.R
+#####
 
+#Setup
 library(tidyverse)
 library(plotly)
 library(readr)
-data(txhousing)
+#data()
+#setwd()
 
+
+#Create UI
 ui = fluidPage(
+  
+  #Input Plotly Object A: State Line Plot
   selectInput(
     inputId = "inputcities",
     label = "Select a city",
@@ -13,9 +22,15 @@ ui = fluidPage(
     multiple = TRUE
   ),
   plotlyOutput(outputId = "outcities")
+  
+  #Input Plotly Object B: Year Histogram
+  #...
 )
 
+#Create Server
 server = function(input, output, ...){
+  
+  #Output Plotly Object A: State Line PLot
   output$outcities = renderPlotly({
     plot_ly(txhousing, x = ~date, y = ~median,color=~city) %>%
       filter(city %in% input$inputcities) %>%
@@ -26,5 +41,11 @@ server = function(input, output, ...){
              xaxis=list(title="Date"),
              yaxis=list(title="Median sale price($))"))
   })
+  
+  #Output Plotly Object B: Year Histogram
+  #...
+  
 }
+
+#Run App
 shinyApp(ui,server)
